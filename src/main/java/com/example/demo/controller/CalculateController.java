4069398978bc803f67b062d253c1f64663fee3f7
Kanayo Noguchi <krt10105@gmail.com>
2022-11-28 18:01:29 +0900

    コントローラー設定2

--- src/main/java/com/example/demo/controller/CalculateController.java
+++ src/main/java/com/example/demo/controller/CalculateController.java
@@ -15,4 +15,27 @@ public class CalculateController {
         model.addAttribute("fdat",new FDat());
         return "tmp_input";
     }
+    @PostMapping("/result")
+    public String result(
+        @ModelAttribute FDat fdat, Model model){
+        calculate(fdat);
+        model.addAttribute("fdat", fdat);
+        return "tmp_result";
+    }
+    private void calculate(FDat fdat){
+        double ans = 0;
+        int num1 = fdat.getNum1();
+        int num2 = fdat.getNum2();
+        String ope = fdat.getOpe();
+        if(ope.equals("+")){
+            ans = num1 + num2;
+        }else if(ope.equals("-")){
+            ans = num1 - num2;
+        }else if(ope.equals("×")){
+            ans = num1 * num2;
+        }else if(ope.equals("÷")){
+            ans = num1 / num2;
+        }
+    fdat.setAns(ans);
+    }
 }
